884b524d2e27ba727f610fcd66e26cb55ed6ad47
Andy McKay <amckay@mozilla.com>
2015-07-21 13:19:52 -0700

    add in redirects

---
+++ docker/nginx/lang.lua
@@ -0,0 +1,83 @@
+--------------------------------------------------
+-- HTTP Accept-Language header handler          --
+-- @author f.ghibellini@gmail.com               --
+--         since altered by Mozilla             --
+-- @license MIT                                 --
+-- @requires:                                   --
+--  -gnu find                                   --
+-- @description:                                --
+--     redirects to subfolders according        --
+--     to the http Accept-Language header       --
+-- @example configuration:                      --
+--                                              --
+--     server {                                 --
+--         listen 8080 default_server;          --
+--         index index.html index.htm;          --
+--         server_name localhost;               --
+--                                              --
+--         set $root /usr/share/nginx/html;     --
+--         root $root;                          --
+--                                              --
+--         location /index.html {               --
+--             # lua_code_cache off;            --
+--             set $default_lang "cz";          --
+--             set $ngx_html_path $root;        --
+--             rewrite_by_lua_file lang.lua;    --
+--         }                                    --
+--     }                                        --
+--                                              --
+--------------------------------------------------
+
+function scandir(directory)
+    local t = {}
+    for filename in io.popen('find "'..directory..'" -type d -mindepth 1 -maxdepth 1 -printf "%f\n"'):lines() do
+        table.insert(t, filename)
+    end
+    return t
+end
+
+function inTable(tbl, item)
+    for key, value in pairs(tbl) do
+        if value == item then return key end
+    end
+    return false
+end
+
+local default_lang = ngx.var.default_lang or "en"
+local folders = scandir(ngx.var.ngx_html_path)
+
+local lang_header = ngx.var.http_accept_language
+if ( lang_header == nil ) then
+    ngx.redirect( "/" .. default_lang .. "/index.html" )
+    return
+end
+
+local cleaned = ngx.re.sub(lang_header, "^.*:", "")
+local options = {}
+local iterator, err = ngx.re.gmatch(cleaned, "\\s*([a-z]+(?:-[a-z])*)\\s*(?:;q=([0-9]+(.[0-9]*)?))?\\s*(,|$)", "i")
+for m, err in iterator do
+    local lang = m[1]
+    local priority = 1
+    if m[2] ~= nil then
+        priority = tonumber(m[2])
+        if priority == nil then
+            priority = 1
+        end
+    end
+    table.insert(options, {lang, priority})
+end
+
+
+table.sort(options, function(a,b) return b[2] < a[2] end)
+
+local redirected = false
+for index, lang in pairs(options) do
+    if inTable(folders, lang[1]) then
+        ngx.redirect( "/" .. lang[1] .. "/index.html" )
+        redirected = true
+        break
+    end
+end
+if not redirected then
+    ngx.redirect( "/" .. default_lang .. "/index.html" )
+end
--- tests/test_status.py
+++ tests/test_status.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import urlparse
 
 import requests
 
@@ -18,3 +19,35 @@ class TestStatuses(unittest.TestCase):
     def test_ui_status(self):
         res = requests.get(UI_URL)
         res.raise_for_status()
+
+
+class TestRedirect(unittest.TestCase):
+
+    def redirect(self, lang):
+        res = requests.get(
+            UI_URL, headers={'Accept-Language': lang}, allow_redirects=False)
+        assert res.status_code == 302
+        return urlparse.urlparse(res.headers['Location']).path
+
+    def test_dbg(self):
+        assert '/dbg/index.html' == self.redirect('dbg')
+
+    def test_en(self):
+        assert '/en/index.html' == self.redirect('en')
+
+    def test_dbg_en(self):
+        assert '/dbg/index.html' == self.redirect('dbg;q=0.7,en;q=0.3')
+
+    def test_fr(self):
+        assert '/en/index.html' == self.redirect('fr')
+
+    def contents(self, lang):
+        res = requests.get(UI_URL, headers={'Accept-Language': lang})
+        assert res.status_code == 200
+        return res.content.strip()
+
+    def test_en_contents(self):
+        assert 'An en page.' == self.contents('en')
+
+    def test_dbg_contents(self):
+        assert 'A dbg page.' == self.contents('dbg')
